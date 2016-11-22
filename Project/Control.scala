d2bfe07555fd45cc8bd5c904eafc9425db525079
raymond <rarias2010@yahoo.com>
2016-11-22 15:00:23 -0800

    Built project so now can run from command line

---
+++ Project/Control.scala
@@ -0,0 +1,12 @@
+import java.util.Scanner
+
+class Control {
+  def Control(){}
+	def main(args: Array[String]) {
+	  val input: Scanner = new Scanner(System.in)
+	  println("Enter your username: ")
+	  val user = input.next()
+	  (new Thread(new Producer(user)).start())
+	  (new Thread(new Consumer()).start())
+	}
+}
---
+++ Project/Producer.scala
@@ -0,0 +1,32 @@
+import org.apache.activemq.ActiveMQConnection;
+import java.net.Socket
+import org.apache.activemq.ActiveMQConnectionFactory
+import javax.jms._
+import java.util.Scanner
+
+class Producer(username : String) extends Runnable {
+	val url: String = "tcp://ec2-35-160-86-191.us-west-2.compute.amazonaws.com:61616"
+			val topicName: String = "chatroom"
+
+			def run() {
+	//val sock = new Socket("ec2-35-162-109-101.us-west-2.compute.amazonaws.com", 61616);
+	val input: Scanner = new Scanner(System.in)
+			val connectionFactory: ConnectionFactory = new ActiveMQConnectionFactory(url)
+			val connection: Connection = connectionFactory.createConnection
+			connection.start
+			val session: Session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE)
+			val destination: Topic = session.createTopic(topicName)
+			println("Connected")
+			val messageProducer: MessageProducer = session.createProducer(destination)
+			val textMessage: TextMessage = session.createTextMessage("Hello Subscriber!")
+			while(true) {
+				val userMessage = input.nextLine()
+						textMessage.setText(userMessage)
+						messageProducer.send(textMessage)
+						//println("Message sent to subscriber: '" + textMessage.getText + "'")
+						println(username + ": " + textMessage.getText)
+			}  
+			connection.close
+}
+}
+
